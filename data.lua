604f2722da506659f8bed0953b715e160e565c01
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-12-29 16:50:53 +0100

    Add generators.

--- data.lua
+++ data.lua
@@ -190,8 +190,7 @@ do
                         line_length = 8,
                         shift = util.by_pixel(0, 2),
                         scale = args.size / base_sprite_size,
-                        hr_version =
-                        {
+                        hr_version = {
                             filename = "__base__/graphics/entity/assembling-machine-1/hr-assembling-machine-1.png",
                             priority="high",
                             width = 214,
@@ -213,8 +212,7 @@ do
                         draw_as_shadow = true,
                         shift = util.by_pixel(8.5, 5.5),
                         scale = args.size / base_sprite_size,
-                        hr_version =
-                        {
+                        hr_version = {
                             filename = "__base__/graphics/entity/assembling-machine-1/hr-assembling-machine-1-shadow.png",
                             priority="high",
                             width = 190,
@@ -243,6 +241,110 @@ do
                 fade_in_ticks = 4,
                 fade_out_ticks = 20
             }
+        }})
+    end
+
+    local function add_composite_generator(args)
+        if #args.ingredients ~= 0 then
+            error("Only generators without ingredients are supported right now.")
+        end
+
+        local full_name = name_prefix .. args.name
+        local composite_factory_entity_name = full_name .. "-generator"
+        local composite_factory_recipe_name = full_name .. "-generator"
+        local composite_factory_item_name = full_name .. "-generator"
+        local base_sprite_size = 3
+        local base_hr_sprite_size = 6
+        local half_size = args.size / 2
+
+        -- Composite factory building item recipe
+        data:extend({{
+            type = "recipe",
+            name = composite_factory_recipe_name,
+            enabled = true,
+            energy_required = 600.0,
+            category = "crafting",
+            ingredients = args.constituent_buildings,
+            results = {
+                {composite_factory_entity_name, 1}
+            }
+        }})
+
+        -- Composite factory item
+        data:extend({{
+            type = "item",
+            name = composite_factory_item_name,
+            icon = "__base__/graphics/icons/solar-panel.png",
+            icon_size = 64,
+            flags = {},
+            subgroup = composite_factory_item_group_name,
+            order = "b",
+            place_result = composite_factory_entity_name,
+            stack_size = 1
+        }})
+
+        data:extend({{
+            type = "electric-energy-interface",
+            name = composite_factory_entity_name,
+            icons = { {icon = "__base__/graphics/icons/solar-panel.png", tint = {r=1, g=0.6, b=0.8, a=1}} },
+            icon_size = 64, icon_mipmaps = 4,
+            flags = {"placeable-neutral", "player-creation"},
+            minable = {mining_time = 1, result = composite_factory_item_name},
+            max_health = 10000,
+            corpse = "medium-remnants",
+            subgroup = "other",
+            collision_box = {{-half_size+0.1, -half_size+0.1}, {half_size-0.1, half_size-0.1}},
+            selection_box = {{-half_size, -half_size}, {half_size, half_size}},
+            gui_mode = "none",
+            energy_source =
+            {
+                type = "electric",
+                buffer_capacity = buffer_size,
+                usage_priority = "tertiary"
+            },
+            energy_production = args.energy_production,
+            energy_usage = "0W",
+            -- also 'pictures' for 4-way sprite is available, or 'animation' resp. 'animations'picture =
+            picture = {
+                layers =
+                {
+                    {
+                        filename = "__base__/graphics/entity/solar-panel/solar-panel.png",
+                        priority = "high",
+                        width = 116,
+                        height = 112,
+                        shift = util.by_pixel(-3, 3),
+                        scale = args.size / base_sprite_size,
+                        hr_version = {
+                            filename = "__base__/graphics/entity/solar-panel/hr-solar-panel.png",
+                            priority = "high",
+                            width = 230,
+                            height = 224,
+                            shift = util.by_pixel(-3, 3.5),
+                            scale = args.size / base_hr_sprite_size
+                        }
+                    },
+                    {
+                        filename = "__base__/graphics/entity/solar-panel/solar-panel-shadow.png",
+                        priority = "high",
+                        width = 112,
+                        height = 90,
+                        shift = util.by_pixel(10, 6),
+                        draw_as_shadow = true,
+                        scale = args.size / base_sprite_size,
+                        hr_version = {
+                            filename = "__base__/graphics/entity/solar-panel/hr-solar-panel-shadow.png",
+                            priority = "high",
+                            width = 220,
+                            height = 180,
+                            shift = util.by_pixel(9.5, 6),
+                            draw_as_shadow = true,
+                            scale = args.size / base_hr_sprite_size
+                        }
+                    }
+                }
+            },
+            vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65}
         }})
     end
 
@@ -285,5 +387,19 @@ do
         size = 12,
         energy_usage = "10MW",
         emissions_per_minute = 1
+    }
+
+    add_composite_generator{
+        name = "generator-test",
+        ingredients = {},
+        energy_production = "100MW",
+        buffer_capacity = "100MJ",
+        constituent_buildings = {
+            {"boiler", 10},
+            {"steam-engine", 20},
+            {"solar-panel", 50},
+            {"inserter", 20}
+        },
+        size = 12
     }
 end
