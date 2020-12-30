a9e980982ecfac9ae5918cdb39a3d6ebcecd8ee5
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-12-30 22:22:54 +0100

    Basic control setup.

--- lib/control.lua
+++ lib/control.lua
@@ -1,5 +1,31 @@
 do
-    cflib = {}
+    global.cflib = {}
 
-    return cflib
+    local function setup_cache()
+        -- TODO: build exchange maps
+    end
+
+    script.on_init(function()
+        setup_cache()
+    end)
+
+    script.on_configuration_changed(function(event)
+        setup_cache()
+    end)
+
+    script.on_event(defines.events.on_gui_opened, function(event)
+        if not event.gui_type == defines.gui_type.entity then
+            return
+        end
+
+        if entity == nil then
+            return
+        end
+
+        -- TODO: the exchange stuff
+
+        game.get_player(entity.player_index).print(entity.name)
+    end, {{filter = "type", type = "container"}, {filter = "name", name = cflib.material_exchange_container}})
+
+    return global.cflib
 end
