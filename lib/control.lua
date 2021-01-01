075421982a7d4c54b970ffe96794d236cae558eb
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-01-01 21:34:14 +0100

    Basic display of the exchange table

--- lib/control.lua
+++ lib/control.lua
@@ -18,29 +18,94 @@ do
         return factories
     end
 
-    local function get_material_exchange_container_gui(player)
-        local material_exchange_container_gui_name = core.make_gui_element_name("material-exchange-container-gui")
+    local function setup_material_exchange_container_gui(player)
+        local gui_name = core.make_gui_element_name("material-exchange-container-gui")
+        local main_pane_name = core.make_gui_element_name("material-exchange-container-gui-main-pane")
+        local exchange_table_name = core.make_gui_element_name("material-exchange-container-gui-exchange-table")
 
-        local gui = player.gui.relative[material_exchange_container_gui_name]
-
-        if gui then
-            return gui
-        end
-
-        player.gui.relative.add{
+        local gui = player.gui.relative.add{
             type = "frame",
-            name = material_exchange_container_gui_name,
+            name = gui_name,
             direction = "vertical",
             caption = "Material Exchange",
             anchor = {
                 gui = defines.relative_gui_type.container_gui,
                 position = defines.relative_gui_position.right,
                 name = core.make_container_name("material-exchange-container")
-            },
-            visible = true
+            }
         }
 
-        return player.gui.relative[material_exchange_container_gui_name]
+        local main_gui_pane = gui.add{
+            type = "scroll-pane",
+            name = main_pane_name,
+            vertical_scroll_policy = "auto-and-reserve-space"
+        }
+
+        local exchange_table = main_gui_pane.add{
+            type = "table",
+            name = exchange_table_name,
+            -- Craft | Show/hide button | Icon/hidden building ingredients | Product summary | Ingredient summary
+            column_count = 5,
+            draw_vertical_lines = true,
+            draw_horizontal_lines = true,
+            draw_horizontal_line_after_header = true,
+            vertical_centering = false
+        }
+
+        local add_exchange_item = function(prototypes)
+            local entity = prototypes.entity
+            local recipe = prototypes.recipe
+            local item = prototypes.item
+            local name = entity.name
+
+            local craft_button_name = core.make_gui_element_name("material-exchange-container-gui-exchange-table-craft-" .. name)
+            local building_ingredients_panel_name = core.make_gui_element_name("material-exchange-container-gui-exchange-table-building-ingredients-panel-" .. name)
+            local toggle_visibility_button_name = core.make_gui_element_name("material-exchange-container-gui-exchange-table-toggle-visibility-button-" .. name)
+
+            exchange_table.add{
+                type = "button",
+                name = craft_button_name,
+                caption = "Craft 1"
+            }
+
+            exchange_table.add{
+                type = "button",
+                name = toggle_visibility_button_name,
+                caption = "S"
+            }
+
+            local building_ingredients_flow = exchange_table.add{
+                type = "table",
+                column_count = 10,
+                name = building_ingredients_panel_name
+            }
+            building_ingredients_flow.add{
+                type = "sprite-button",
+                enabled = false,
+                sprite = "item/" .. item.name,
+                number = 12345
+            }
+
+            local product_summary_flow = exchange_table.add{
+                type = "flow",
+                direction = "vertical"
+            }
+
+            local ingredient_summary_flow = exchange_table.add{
+                type = "flow",
+                direction = "vertical"
+            }
+        end
+
+        add_exchange_item(global.cflib.prototypes[1])
+
+        return gui
+    end
+
+    local function get_material_exchange_container_gui(player)
+        local material_exchange_container_gui_name = core.make_gui_element_name("material-exchange-container-gui")
+
+        return player.gui.relative[material_exchange_container_gui_name] or setup_material_exchange_container_gui(player)
     end
 
     local function setup_cache()
